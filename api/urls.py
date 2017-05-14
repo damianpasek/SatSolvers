1dd6b48bc759728405ebf3cff00eec151942db75
Damian Pasek <damianpasek@gmail.com>
2017-05-14 17:29:22 +0200

    Create solvers API endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from api.views import IndexView
+from api.views import IndexView, SolversList
 
 urlpatterns = [
     url(r'^$', IndexView.as_view()),
+    url(r'^solvers/$', SolversList.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -2,9 +2,19 @@ import time
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from backend.models import Solver
+from backend.serializers import SolverSerializer
+
 
 class IndexView(APIView):
     def post(self, request):
         data = {'test': 5}
         time.sleep(5)
         return Response(data)
+
+
+class SolversList(APIView):
+    def get(self, request):
+        solvers = Solver.objects.all()
+        serializer = SolverSerializer(solvers, many=True)
+        return Response(serializer.data)
